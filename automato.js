68560368e9f11d149c786e00b945658041d5c5eb
Kelwin Komka <kelwin_komka@hotmail.com>
2019-10-30 00:13:09 -0300

    Modificação pare leitura da palavra

--- automato.js
+++ automato.js
@@ -5,26 +5,29 @@ var lastCellId = "";
 var currentCellId = "";
 
 function palavraChange(valor) {
-    if (valor.trim().length > 0){
+    if (valor.trim().length > 0 && estados.length > 0){
         var letra = valor[valor.length - 1].toLowerCase();
         var estado = estados[indexEstado];
         var index = letra.charCodeAt() - 97;
         console.log("letra="+letra+", estado="+estado+", index="+index);
         document.getElementById("inputPalavra").style.color = "black";
         if (index >= 0) {
-            var proximoIndex = estado[index];
+            let proximoIndex = estado[index];
             console.log("proximoIndex="+proximoIndex);
             if (proximoIndex) {
                 lastCellId = currentCellId;
-                if (lastCellId.trim().length > 0)
+                if (lastCellId.trim().length > 0) {
                     document.getElementById(lastCellId).style.backgroundColor = "#EBF5FF";
-                currentCellId = String(indexEstado)+String(index);
+                }
+                currentCellId = "td"+String(indexEstado)+"."+String(index);
 
                 console.log("lastCellId="+lastCellId+", currentCellId="+currentCellId);
 
                 indexEstado = proximoIndex == -1 ? 0 : proximoIndex;
 
-                document.getElementById(currentCellId).style.backgroundColor = "#409d00";
+                if (indexEstado >= 0) {
+                    document.getElementById(currentCellId).style.backgroundColor = "#99ccff";
+                }
             } else
                 document.getElementById("inputPalavra").style.color = "red";
         } else if (index == -65) {
@@ -34,6 +37,13 @@ function palavraChange(valor) {
             lastCellId = "";
             currentCellId = "";
         }             
+    } else {
+        document.getElementById("inputPalavra").style.color = "black";
+        indexEstado = 0;
+        document.getElementById(lastCellId).style.backgroundColor = "#EBF5FF";
+        document.getElementById(currentCellId).style.backgroundColor = "#EBF5FF";
+        lastCellId = "";
+        currentCellId = "";
     }
 }
 
@@ -44,10 +54,12 @@ function preencherLista(){
     }
     var i = 0;
 
+    document.getElementById("inputPalavra").style.color = "black";
+    indexEstado = 0;
     estados = new Array;
     for (i in listaTokens){
-        var token = listaTokens[i];
-        var buttonItem = document.createElement("BUTTON");
+        let token = listaTokens[i];
+        let buttonItem = document.createElement("BUTTON");
         buttonItem.className = "btn";
         buttonItem.innerHTML = token;
         buttonItem.addEventListener("click", function(){
@@ -57,8 +69,8 @@ function preencherLista(){
 
         divTokens.appendChild(buttonItem);
 
-        var estadosValor = new Array(token.length);
-        var j = 0;
+        let estadosValor = new Array(token.length);
+        let j = 0;
         for (j in token){
             var letra = token[j].toLowerCase();
             var letraIndex = letra.charCodeAt() - 97;
@@ -94,22 +106,23 @@ function preencherLista(){
     tableAutomato.appendChild(tableRow);
 
     for (i in estados){
-        var estado = estados[i];
+        let estado = estados[i];
         tableRow = document.createElement("tr");
+        tableRow.id = "tr"+i;
 
-        var tableCell = document.createElement("td");
+        let tableCell = document.createElement("td");
         tableCell.innerHTML = "q"+i;
         tableRow.appendChild(tableCell);
         
         for (j = 0; j < 26; j++){
             tableCell = document.createElement("td");
-            var valor = estado[j];
+            let valor = estado[j];
             tableCell.innerHTML = "-";
             if (valor == -1)
                 tableCell.innerHTML = "\u03B5";
             else if (valor)
                 tableCell.innerHTML = "q"+valor;
-            tableCell.id = i+j
+            tableCell.id = "td"+i+"."+j
             tableRow.appendChild(tableCell);
         }
         tableAutomato.appendChild(tableRow);
@@ -117,9 +130,15 @@ function preencherLista(){
 }
 
 function adicionarListaTokens(valor){
-    if (valor && valor.trim().length > 0 && listaTokens.indexOf(valor) == -1){
+    if (valor && valor.trim().length > 0 && 
+            listaTokens.indexOf(valor) == -1 && 
+            valor.search(/[^a-zA-Z]+/) === -1){
         listaTokens.push(valor);
         preencherLista();
+    } else {
+        alert("Apenas letras de \"a\" a \"z\" são permitidas!");
+        document.getElementById("inputToken").value = "";
     }
-    document.getElementById("inputToken").value = "";
 }
+
+preencherLista();
